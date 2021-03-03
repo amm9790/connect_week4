bdb2488cb0ee40a9aff5c4a189429ded22578074
Ahmed El Shaer <74311769+amm9790@users.noreply.github.com>
2021-03-03 01:40:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
+//express is a node modoule for bulding HTTP servers
+const { resolveSoa } = require('dns');
+var datastore = require('nedb');
+var db = new datastore({ filename: 'database.json', autoload: true });
 var express = require('express');
 var app = express();
-app.use(express.static('public'));
 
 
-app.get('/', function (req,res){
-    res.send('Hello World! Adding  new change')
+app.use(express.static("public"));
+
+app.get('/formdata', function(req, res){
+    console.log(req.query.text);
+
+var dataToSave = {
+    text: req.query.text,
+    color: req.query.color
+};
+// console.log(dataToSave);
+// submittedData.push(dataToSave);
+
+db.insert(dataToSave, function (err, newDoc) {  
+    db.find({}, function (err, docs) {
+        // var dataWrapper = {data: docs};
+        // res.render("outputtemplate.ejs",dataWrapper )
+    res.send(docs);
+      });
+  });
+
 });
 
-app.listen(80, function(){
-console.log('Example app listening on port 80!');
-});
+    app.listen(80, function (){
+        console.log('Example app listening on part 80!')
+    })
